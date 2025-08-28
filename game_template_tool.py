14bf904bc2670d1337b365bc1822118e8547fe37
Felipe <91634101+Felipe-Fortes@users.noreply.github.com>
2025-08-28 05:27:18 -0300

    Criado

---
+++ game_template_tool.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import messagebox 
+
+def criar_snake():
+    messagebox.showinfo("Blueprint", "Jogo Snake Sendo Gerado...")
+
+def criar_campo_minado():
+    messagebox.showinfo("Blueprint", "Jogo Campo Minado Sendo Gerado...")
+
+def main():
+    root = tk.Tk()
+    root.title("Ferramenta de Tempplates")
